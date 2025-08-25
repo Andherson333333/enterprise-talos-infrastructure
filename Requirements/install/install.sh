4722ba7eaf699230be2ab159cb4db5433852b0d0
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-08-25 19:01:30 -0400

    Create install.sh

---
+++ Requirements/install/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Iniciando instalación de herramientas..."
+
+# Actualizar repositorios
+sudo apt-get update
+
+# Instalar dependencias
+sudo apt-get install -y curl unzip
+
+# Instalar kubectl
+echo "Instalando kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Instalar Terraform
+echo "Instalando Terraform..."
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update
+sudo apt-get install -y terraform
+
+# Instalar Helm
+echo "Instalando Helm..."
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+echo "Instalación completada. Verificando versiones:"
+kubectl version --client
+terraform --version
+aws --version
+helm version
