0a7ee7eaf63b6f12a4e3b25152306951180ee343
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-08-26 17:53:22 -0400

    Create provider.tf

---
+++ terraform/proxmox- connection/provider.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.80.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.10"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "~> 0.8.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure  = var.proxmox_insecure
+}
