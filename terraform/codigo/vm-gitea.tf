e5be650d9a742d6d3ba5675011c14fbe02cd452e
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-09-03 11:59:27 -0400

    Create vm-gitea.tf

---
+++ terraform/codigo/vm-gitea.tf
@@ -0,0 +1,46 @@
+resource "proxmox_virtual_environment_vm" "gitea_server" {
+  node_name = var.node_name
+  name      = "gitea-server"
+
+  clone {
+    vm_id = 8001
+    full  = true
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    interface      = "scsi0"
+    datastore_id   = "local-lvm"
+    size           = 20
+    discard        = "on"
+  }
+
+  disk {
+    interface      = "scsi1"
+    datastore_id   = "local-lvm"
+    size           = 20
+    file_format    = "raw"
+    discard        = "on"
+  }
+
+  boot_order = ["scsi0"]
+
+  network_device {
+    bridge = "vmbr1"
+    model  = "virtio"
+  }
+
+  started = true
+  stop_on_destroy = true
+
+  tags = ["terraform", "infrastructure"]
+}
