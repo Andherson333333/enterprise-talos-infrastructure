f66ef553e2f2a30ab3a510d0cf37391106c7ab86
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-09-03 12:00:44 -0400

    Create vm-gitea-runners.tf

---
+++ terraform/codigo/vm-gitea-runners.tf
@@ -0,0 +1,40 @@
+resource "proxmox_virtual_environment_vm" "gitea_runner" {
+  count     = var.gitea_count_runners
+  node_name = var.node_name
+  name      = "${var.gitea_runners}-${format("%02d", count.index + 1)}"
+
+  clone {
+    vm_id = 8000
+    full  = true
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    interface      = "scsi0"
+    datastore_id   = "local-lvm"
+    size           = 30
+    discard        = "on"
+  }
+
+  boot_order = ["scsi0"]
+
+  network_device {
+    bridge = "vmbr1"
+    model  = "virtio"
+  }
+
+  started = true
+  stop_on_destroy = true
+
+  # Tags para organización
+  tags = ["terraform", "infrastructure", "gitea", "runner"]
+}
